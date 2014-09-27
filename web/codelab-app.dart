1036c00672bba311c1fe2adeeb8ede700215354f
Jakub Škvára <jskvara@gmail.com>
2014-09-27 18:03:35 +0200

    Step 4 - added notes list

--- web/codelab-app.dart
+++ web/codelab-app.dart
@@ -1,7 +1,41 @@
 import 'package:polymer/polymer.dart';
 
+class Note extends Observable {
+
+   @observable String body;
+
+   @observable bool done;
+
+   Note([this.body = "", this.done = false]);
+}
+
 @CustomTag('codelab-app')
 class CodelabApp extends PolymerElement {
+
+  @observable String newNote = "";
+  
+  @observable List data = toObservable([]);
+
   CodelabApp.created() : super.created() {
+  }
+  
+  ready() {
+    $['newNoteInput'].style.display = 'none';
+  }
+
+  toggleDrawer() {
+    $['drawerPanel'].togglePanel();
+  }
+
+  showNewNoteInput() {
+    $['newNoteInput'].style.display = 'block';
+  }
+
+  add() {
+    if (newNote != null && newNote != "") {
+      data.add(new Note(newNote));
+      $['newNoteInput'].style.display = 'none';
+      $['newNoteInput'].value = null;
+    }
   }
 }
