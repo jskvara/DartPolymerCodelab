7b3765d1fd0f816d8f2a98ec6967ff62db478a5f
Jakub Škvára <jskvara@gmail.com>
2014-09-27 17:00:39 +0200

    First step - added codelab-app element

---
+++ web/codelab-app.dart
@@ -0,0 +1,8 @@
+//import 'dart:convert';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('codelab-app')
+class CodelabApp extends PolymerElement {
+  CodelabApp.created() : super.created() {
+  }
+}
